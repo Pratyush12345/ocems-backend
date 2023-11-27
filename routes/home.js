05c8794d5e483826bd640ee8a89265ca726ae653
Pratyush Gupta <Pratyush12345>
2023-11-27 18:45:02 +0530

    Initial Main Commit

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get("/", (req, res)=>{
+    res.status(200).send("Ocems Server")
+})
 router.use('/user', require('./user'))
 router.use('/admin', require('./admin'))
 router.use('/officer', require('./officer'))
