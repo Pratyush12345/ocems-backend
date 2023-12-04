ab46cdb595f958eea621933a1f56660292c6b91b
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-12-04 15:10:46 +0530

    Update routes

--- routes/industry.js
+++ routes/industry.js
@@ -4,17 +4,10 @@ const checkAuth = require('../middlewares/check-auth')
 const industryController = require('../controllers/industryController')
 
 router.post('/signup', industryController.signUp)
-
-// router.get('/', checkAuth, industryController.getRequests)
-// router.post('/approve', checkAuth, industryController.approveRequest)
-// router.post('/reject', checkAuth, industryController.rejectRequest)
-// router.post('/bulkadd', checkAuth, industryController.bulkUpload)
-// router.delete('/delete/:uid', checkAuth, industryController.deleteIndustry)
-
-router.get('/', industryController.getRequests)
-router.post('/approve/:uid', industryController.approveRequest)
-router.post('/reject/:uid', industryController.rejectRequest)
-router.post('/bulkadd', industryController.bulkUpload)
-router.delete('/delete/:uid', industryController.deleteIndustry)
+router.get('/', checkAuth, industryController.getRequests)
+router.post('/approve', checkAuth, industryController.approveRequest)
+router.post('/reject', checkAuth, industryController.rejectRequest)
+router.post('/bulkadd', checkAuth, industryController.bulkUpload)
+router.delete('/delete/:uid', checkAuth, industryController.deleteIndustry)
 
 module.exports = router
