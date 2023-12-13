eae4e0e95923eef38fa51d63c4a70a66edea91f5
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-12-14 02:33:32 +0530

    Fix params

--- routes/industry/billMaster.js
+++ routes/industry/billMaster.js
@@ -6,6 +6,6 @@ const checkAuth = require('../../middlewares/check-auth')
 router.get('/', checkAuth, billMasterController.getMasterCopies)
 router.post('/create', checkAuth, billMasterController.createCopy)
 router.patch('/update', checkAuth, billMasterController.updateCopy)
-router.delete('/delete', checkAuth, billMasterController.deleteCopy)
+router.delete('/delete/:id', checkAuth, billMasterController.deleteCopy)
 
 module.exports = router
