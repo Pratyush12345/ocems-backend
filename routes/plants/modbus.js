ee7cfa65e47158438777a4fcc6acb78f74d76ae8
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2024-01-02 15:00:38 +0530

    Fix modbus errors

--- routes/plants/modbus.js
+++ routes/plants/modbus.js
@@ -3,15 +3,10 @@ const router = express.Router()
 const modbusController = require('../../controllers/plant/modbusController')
 const checkAuth = require('../../middlewares/check-auth')
 
-
-router.get('/', modbusController.getAllAddress)
-
-router.get('/report', modbusController.getReport)
-
-router.post('/add', modbusController.addInstrumentsModbusAddress)
-router.patch('/update/tagno', modbusController.updateTagNo)
-router.delete('/delete/:address', modbusController.deleteAddress)
-
-// router.post('/test', modbusController.reporter)
+router.get('/', checkAuth, modbusController.getAllAddress)
+router.get('/report', checkAuth, modbusController.getReport)
+router.post('/add', checkAuth, modbusController.addInstrumentsModbusAddress)
+router.patch('/update/tagno', checkAuth, modbusController.updateTagNo)
+router.delete('/delete/:address', checkAuth, modbusController.deleteAddress)
 
 module.exports = router
